16bbf264ee370484a07957b82cc033806dda466e
Jainam Shah <sjainam141@gmail.com>
2026-07-14 13:52:30 +0530

    feat: implement demand forecasting models and future forecasting (fixes #10, fixes #11, fixes #12, fixes #13)

---
+++ src/forecasting.py
@@ -0,0 +1,135 @@
+"""
+Forecasting Module
+
+Reusable forecasting functions for the Supply Chain Demand
+Forecasting project.
+"""
+
+import numpy as np
+import pandas as pd
+
+from statsmodels.tsa.arima.model import ARIMA
+
+
+def train_test_split_time_series(df, train_size=0.8):
+    """
+    Split a time-series dataset into train and test sets.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        Input dataframe.
+
+    train_size : float
+        Fraction of data to use for training.
+
+    Returns
+    -------
+    train : DataFrame
+    test : DataFrame
+    """
+
+    split_index = int(len(df) * train_size)
+
+    train = df.iloc[:split_index]
+
+    test = df.iloc[split_index:]
+
+    return train, test
+
+
+def moving_average_forecast(train, test, column="sales", window=7):
+    """
+    Forecast using a simple moving average.
+
+    Parameters
+    ----------
+    train : DataFrame
+
+    test : DataFrame
+
+    column : str
+
+    window : int
+
+    Returns
+    -------
+    list
+        Forecast values.
+    """
+
+    history = list(train[column])
+
+    predictions = []
+
+    for actual in test[column]:
+
+        prediction = np.mean(history[-window:])
+
+        predictions.append(prediction)
+
+        history.append(actual)
+
+    return predictions
+
+
+def train_arima(train, column="sales", order=(5, 1, 0)):
+    """
+    Train an ARIMA model.
+
+    Returns
+    -------
+    Fitted ARIMA model.
+    """
+
+    model = ARIMA(train[column], order=order)
+
+    return model.fit()
+
+
+def arima_forecast(model, steps):
+    """
+    Forecast future observations.
+
+    Parameters
+    ----------
+    model : fitted ARIMA model
+
+    steps : int
+
+    Returns
+    -------
+    Forecast values
+    """
+
+    return model.forecast(steps=steps)
+
+
+def forecast_future(df, column="sales", periods=90, order=(5, 1, 0)):
+    """
+    Forecast future demand.
+
+    Parameters
+    ----------
+    df : DataFrame
+
+    periods : int
+
+    Returns
+    -------
+    Forecast DataFrame
+    """
+
+    model = ARIMA(df[column], order=order)
+
+    fitted_model = model.fit()
+
+    forecast = fitted_model.forecast(steps=periods)
+
+    future_dates = pd.date_range(
+        start=df.index[-1] + pd.Timedelta(days=1), periods=periods, freq="D"
+    )
+
+    forecast_df = pd.DataFrame({"date": future_dates, "forecast_sales": forecast})
+
+    return forecast_df
